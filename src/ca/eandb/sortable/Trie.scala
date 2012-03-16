2a961026bd83506e6d5b293cd468b9b8b4976669
Brad Kimmel <intrepidca@gmail.com>
2012-03-16 16:39:08 -0400

    Changed data field to Option[T] (from Set[T]), fixes to insert  - Insert should only create new child if one does not already exist for    that character.  - Added method to get the data -- with option to specify default.  - Added fields for parent and character.

--- src/ca/eandb/sortable/Trie.scala
+++ src/ca/eandb/sortable/Trie.scala
@@ -9,15 +9,23 @@ import scala.collection.mutable._
  * @author Brad Kimmel
  *
  */
-final class Trie[T] {
+final class Trie[T](val parent: Trie[T] = null, val char: Char = '\0') {
   
   private var children: Map[Char, Trie[T]] = Map.empty
-  val data: Set[T] = Set.empty
+  var data: Option[T] = None
   
-  def insert(c: Char) : Trie[T] = {
-    val child = new Trie[T]
-    children += c -> child
-    child
+  def get(default: => T) : T = data match {
+    case Some(x) => x
+    case None => { data = Some(default); data.get }
+  }
+  
+  def insert(c: Char) : Trie[T] = children get c match {
+    case Some(child) => child
+    case None => { 
+      val child = new Trie[T](this, c)
+      children(c) = child
+      child
+    }
   }
   
   def insert(s: String) : Trie[T] = (this /: s)(_.insert(_))
