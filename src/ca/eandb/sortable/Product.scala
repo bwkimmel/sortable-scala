540b6f670938ff7d4cb74e77039842db32bf8844
Brad Kimmel <intrepidca@gmail.com>
2012-03-16 07:47:26 +0000

    Initial checkin of Product entity class.

---
+++ src/ca/eandb/sortable/Product.scala
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package ca.eandb.sortable
+
+/**
+ * @author Brad Kimmel
+ *
+ */
+final class Product(
+    val productName : String,
+    val manufacturer : String,
+    val model : String,
+    val family : String,
+    val announcedDate : String) {
+
+}
